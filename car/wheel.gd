0e12ddc46dd8b657b6efb30f1ff5964a1b800a4b
Karum Ghag <karumghag09@gmail.com>
2025-06-14 13:47:59 +0100

    Added lateral friction

--- car/wheel.gd
+++ car/wheel.gd
@@ -1,6 +1,6 @@
 extends RayCast3D
 
-class_name WheelClass
+class_name Wheels
 
 @export var car : CarClass
 
@@ -8,30 +8,61 @@ var restLen: float = 1.5
 var length : float
 var extension : float
 var collisionPoint : Vector3
-var springConst : float = 300.0
-var damping: float = 40.0
-var maxForce: float = 1000.0
 
-var upDir : Vector3 = Vector3.UP
+var springConst : float = 300.0
+var damping: float = 30.0
+
+var upDir : Vector3 = global_transform.basis.y.normalized()
+var forwardDir = global_transform.basis.z.normalized()   # Assuming Z is forward
+var lateralDir = upDir.cross(forwardDir).normalized() 
+
+
 var origin : Vector3 = Vector3.ZERO
 
+var maxForce: float = 1000.0
 var springForce : float
 var dampForce : float
-var overallForce : float
+var overallForce : Vector3
 var forceVec : Vector3
 var speed : float
 
 var applyPos : Vector3
 
+
+#------LateralFriction------
+var wheelVelocity : Vector3
+var lateralSpeed : float
+
+var grip : float = 500
+
+var lateralFrictionForce : Vector3
+
+
 func _physics_process(_delta):
 	if not is_colliding():
 		return
 	
+	#------Suspension------
+
+	upDir = global_transform.basis.y.normalized()
+
+	#Vector3(0, 1, 0).cross(Vector(1, 0 ,0 )) returns Vector3(0, 0, -1)
+	wheelVelocity = car.linear_velocity + car.angular_velocity.cross(applyPos)
+
+	lateralSpeed = wheelVelocity.dot(lateralDir)
+
+	lateralFrictionForce = -lateralSpeed * grip * lateralDir
+
+
+
 	#gets positon of the wheel and the position relative to the car
 	origin = global_position
 	applyPos = origin - car.global_position
 	#where the ray cast collided
 	collisionPoint = get_collision_point()
+
+
+
 
 	#gets the length of the raycast
 	length = (origin - collisionPoint).length()
@@ -47,10 +78,14 @@ func _physics_process(_delta):
 	dampForce = -speed * damping
 
 	#calculates overall force and limits it, turns it into a vector 3
-	overallForce = clamp(springForce + dampForce, -maxForce, maxForce)
-	forceVec = upDir * overallForce
+	springForce = clamp(springForce + dampForce, -maxForce, maxForce)
+	overallForce = (upDir * springForce) + lateralFrictionForce
+
+
 
 	#applies it, applyPos is needed bc apply_force() second param takes it relative to the location of the rigidbody not at a specific point in world
-	car.apply_force(forceVec, applyPos)
+	car.apply_force(overallForce, applyPos)
 
+
+	#print(overallForce)
 	
